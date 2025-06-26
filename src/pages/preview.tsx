783b435d2bdc069b782ab4eb230459c8be2379a7
Pranto Deb <rajprit0m@outlook.com>
2025-06-26 16:59:41 +0600

    [#]

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -1,5 +1,9 @@
+import ClassicTemplate from "@/components/templetes/classicTemplate";
+import CreativeTemplate from "@/components/templetes/creativetemplate";
+import MinimalTemplate from "@/components/templetes/minimalTemplate";
+import ModernTemplate from "@/components/templetes/modernTemplate";
 import type { ResumeData } from "@/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Preview = () => {
@@ -13,6 +17,32 @@ const Preview = () => {
 
   console.log("Template ID:", templateId);
   console.log("Resume ID:", resumeId);
+
+  useEffect(() => {
+    if (templateId) {
+      setTemplate(templateId);
+    }
+
+    setLoading(false);
+  }, [templateId]);
+
+  const renderTemplate = () => {
+    if (!resumeData) return null;
+
+    switch (template) {
+      case "modern":
+        return <ModernTemplate data={resumeData} />;
+      case "classic":
+        return <ClassicTemplate data={resumeData} />;
+      case "creative":
+        return <CreativeTemplate data={resumeData} />;
+      case "minimal":
+        return <MinimalTemplate data={resumeData} />;
+      default:
+        return <ModernTemplate data={resumeData} />;
+    }
+  };
+
   return <div></div>;
 };
 
