aa3e18dfc2a434659ed8a8e49d3ca3a1edba9654
Pranto Deb <rajprit0m@outlook.com>
2025-06-27 19:55:59 +0600

    [#]

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user.slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistedAuthReducer = persistReducer(
+  {
+    key: "auth",
+    storage,
+  },
+  userReducer
+);
 
 export const store = configureStore({
   reducer: {
-    auth: userReducer,
+    auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persisted = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
