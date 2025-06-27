04cafa288ac211766cba56afa0bd5c5bf72ebca2
Pranto Deb <rajprit0m@outlook.com>
2025-06-27 22:09:10 +0600

    [Login]

--- src/firebase/authService.ts
+++ src/firebase/authService.ts
@@ -7,6 +7,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth } from "./config";
+import type { User } from "firebase/auth";
 
 export const signInWithGoogle = () => {
   const provider = new GoogleAuthProvider();
@@ -25,6 +26,6 @@ export const logout = () => {
   return signOut(auth);
 };
 
-export const onAuthChange = (callback: (user: any) => void) => {
+export const onAuthChange = (callback: (user: User | null) => void) => {
   return onAuthStateChanged(auth, callback);
 };
