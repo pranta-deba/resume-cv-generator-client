70d025624858fc405ac8d44fc07f1291be37ea3d
Pranto Deb <rajprit0m@outlook.com>
2025-06-27 20:19:14 +0600

    [#]

---
+++ src/firebase/authService.ts
@@ -0,0 +1,26 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./config";
+
+export const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
+};
+
+export const loginWithEmail = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const registerWithEmail = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+
+export const logout = () => {
+  return signOut(auth);
+};
