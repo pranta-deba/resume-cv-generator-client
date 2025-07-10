c8c600a06b22e040b76462b75d57a62443149cea
Pranto Deb <rajprit0m@outlook.com>
2025-07-10 15:51:43 +0600

    [#]

--- src/pages/builder.tsx
+++ src/pages/builder.tsx
@@ -1,11 +1,13 @@
-
+import { useSearchParams } from "react-router-dom";
 
 const Builder = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [searchParams] = useSearchParams();
+  const templateId = searchParams.get("template");
+  const resumeId = searchParams.get("id");
+
+  console.log("Template ID:", templateId);
+  console.log("Resume ID:", resumeId);
+  return <div>builders</div>;
 };
 
 export default Builder;
