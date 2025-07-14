b7ceafa6c3b08d4934d01382cbcab5a1f055c551
Pranto Deb <rajprit0m@outlook.com>
2025-07-14 21:25:10 +0600

    [Dashboard]

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "./button";
 import { useAppSelector } from "@/redux/hooks";
 import { selectUser } from "@/redux/features/user.slice";
